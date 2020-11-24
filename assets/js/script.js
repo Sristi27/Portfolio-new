5a7f389a16934c4fdf626fc30eb24b1c3cce53f1
Sristi27 <sristi2705@gmail.com>
2020-11-25 02:06:39 +0530

    changed cv

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,7 @@
-function submit(e)
+function submitForm()
 {
 
- e.preventDefault();
- var input1=document.getElementById("name");
- var input2=document.getElementById("email");
- var input3=document.getElementById("message");
- var div1=document.getElementsByClassName("sent-message")[0];
-
- div1.style.display="block";
- input1.innerHTML="";
- input2.innerHTML="";
- input3.innerHTML="";
+ alert("Thank you")
  
 
 }
